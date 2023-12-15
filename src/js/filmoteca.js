423dad0a570beed8d566075b1acb4119eeb35562
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-12-15 12:13:36 +0200

    Update filmoteca.js

--- src/js/filmoteca.js
+++ src/js/filmoteca.js
@@ -21,37 +21,36 @@ let page = 1;
 
 loadMore.addEventListener('click', onLoadMore);
 
-// тут за замовчуванням page = 1;
+// serviceMovie()
+//   .then(data => {
+//     // console.log(data.page);
+//     container.insertAdjacentHTML('beforeend', createMarkup(data.results));
 
-serviceMovie()
-  .then(data => {
-    // console.log(data.page);
-    container.insertAdjacentHTML('beforeend', createMarkup(data.results));
+//     if (data.page < data.total_pages) {
+//       // data.page < 500
+//       loadMore.classList.replace('load-more-hidden', 'load-more');
+//     }
+//   })
+//   .catch(error => console.log('Error', error));
 
-    if (data.page < data.total_pages) {
-      // data.page < 500
-      loadMore.classList.replace('load-more-hidden', 'load-more');
-    }
-  })
-  .catch(error => console.log('Error', error));
+//            тут за замовчуванням page = 1;
+// function serviceMovie(page = 1) {
+//   const BASE_URL = 'https://api.themoviedb.org/3';
+//   const END_POINT = '/trending/movie/week';
+//   const API_KEY = '345007f9ab440e5b86cef51be6397df1';
 
-function serviceMovie(page = 1) {
-  const BASE_URL = 'https://api.themoviedb.org/3';
-  const END_POINT = '/trending/movie/week';
-  const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+//   const queryParams = new URLSearchParams({
+//     api_key: API_KEY,
+//     page: page,
+//   });
 
-  const queryParams = new URLSearchParams({
-    api_key: API_KEY,
-    page: page,
-  });
-
-  return fetch(`${BASE_URL}${END_POINT}?${queryParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-}
+//   return fetch(`${BASE_URL}${END_POINT}?${queryParams}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
+//     return response.json();
+//   });
+// }
 
 function createMarkup(arr) {
   return arr
@@ -81,120 +80,65 @@ function onLoadMore() {
     }
   });
 }
-
 // -------------------------------------------------------------
-// let page = 1;
-
-// loadMore.addEventListener("click", onLoadMore)
-
-// serviceMovie()
-//     .then(data => {
-//         container.insertAdjacentHTML("beforeend", createMarkup(data.results));
-
-//         if(data.page < data.total_pages) { // data.page < 500
-//             loadMore.classList.replace("load-more-hidden", "load-more")
-//         }
-//     })
-//     .catch(error => console.log("Error!!!!", error))
+//                      те саме тільки через  axios
 
 // function serviceMovie(page = 1) {
-//     const BASE_URL = "https://api.themoviedb.org/3";
-//     const END_POINT = "/trending/movie/week";
-//     const API_KEY = "345007f9ab440e5b86cef51be6397df1";
+//   const BASE_URL = 'https://api.themoviedb.org/3';
+//   const END_POINT = '/trending/movie/week';
+//   const API_KEY = '345007f9ab440e5b86cef51be6397df1';
 
-//     const queryParams = new URLSearchParams({
-//         api_key: API_KEY,
-//         page: page
+//   const queryParams = new URLSearchParams({
+//     api_key: API_KEY,
+//     page: page,
+//   });
+//   return axios
+//     .get(`${BASE_URL}${END_POINT}?${queryParams}`)
+//     .then(resp => {
+//       console.log(resp);
+//       return resp.data;
 //     })
-
-//     return fetch(`${BASE_URL}${END_POINT}?${queryParams}`)
-//         .then(resp => {
-//             if(!resp.ok) {
-//                 throw new Error(resp.statusText)
-//             }
-//             return resp.json()
-//         })
+//     .catch(error => {
+//       throw new Error(error);
+//     });
 // }
 
-// function createMarkup(arr) {
-//     return arr.map(({ poster_path, release_date, original_title, vote_average }) => `
-//         <li class="movie-card">
-//             <img src="https://image.tmdb.org/t/p/w300${poster_path}" alt="${original_title}">
-//             <div class="movie-info">
-//                 <h2>${original_title}</h2>
-//                 <p>Release date: ${release_date}</p>
-//                 <p>Vote average: ${vote_average}</p>
-//             </div>
-//         </li>
-//     `).join("")
-// }
+// ------------------------------------------------------------
+//  те саме тільки через async await
 
-// function onLoadMore() {
-//     page += 1;
+async function render() {
+  try {
+    const data = await serviceMovie();
 
-//     serviceMovie(page)
-//         .then(data => {
-//             container.insertAdjacentHTML("beforeend", createMarkup(data.results))
+    container.insertAdjacentHTML('beforeend', createMarkup(data.results));
+    if (data.page < data.total_pages) {
+      // data.page < 500
+      loadMore.classList.replace('load-more-hidden', 'load-more');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+render();
 
-//             if(data.page >= data.total_pages) { // data.page >=
-//                 loadMore.classList.replace("load-more", "load-more-hidden")
-//             }
-//         })
-// }
+async function serviceMovie(page = 1) {
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const END_POINT = '/trending/movie/week';
+  const API_KEY = '345007f9ab440e5b86cef51be6397df1';
+  const queryParams = new URLSearchParams({
+    api_key: API_KEY,
+    page: page,
+  });
+  try {
+    const response = await axios.get(`${BASE_URL}${END_POINT}?${queryParams}`);
+    return await response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-// function serviceMovie(page = 1) {
-//     const BASE_URL = "https://api.themoviedb.org/3";
-//     const END_POINT = "/trending/movie/week";
-//     const API_KEY = "345007f9ab440e5b86cef51be6397df1";
-
-//     const queryParams = new URLSearchParams({
-//         api_key: API_KEY,
-//         page: page
-//     })
-
-//     return axios.get(`${BASE_URL}${END_POINT}?${queryParams}`)
-//         .then(resp => {
-//             return resp.data
-//         })
-//         .catch(error => {
-//             throw new Error(error)
-//         })
-// }
-
-// async function render() {
-//     try {
-//         const data = await serviceMovie()
-//         container.insertAdjacentHTML("beforeend", createMarkup(data.results));
-
-//         if(data.page < data.total_pages) { // data.page < 500
-//             loadMore.classList.replace("load-more-hidden", "load-more")
-//         }
-//     } catch(error) {
-//         console.log("Error!!!!", error);
-//     }
-// }
-// render()
-
-// async function serviceMovie(page = 1) {
-//     const BASE_URL = "https://api.themoviedb.org/3";
-//     const END_POINT = "/trending/movie/week";
-//     const API_KEY = "345007f9ab440e5b86cef51be6397df1";
-
-//     const queryParams = new URLSearchParams({
-//         api_key: API_KEY,
-//         page: page
-//     })
-
-//     try {
-//         const res = await axios.get(`${BASE_URL}${END_POINT}?${queryParams}`)
-//         return await res.data
-//     } catch(error) {
-//         throw new Error(error)
-//     }
-
-// }
-
-// serviceMovie()
+serviceMovie();
+// -------------------------------------------------------------
 
 // ********************************Infinity scroll ********************** \\
 
